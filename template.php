3e9c8cb56a33b1fa5fc6677491552378638fe9c5
Pez Cuckow <email@pezcuckow.com>
2021-02-16 09:21:35 +0000

    Add HTTPS upgrade

--- template.php
+++ template.php
@@ -2,6 +2,8 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
         <title>Peg Productions Limited</title>
         <meta name="description" content="<?php echo $description; ?>">
         
